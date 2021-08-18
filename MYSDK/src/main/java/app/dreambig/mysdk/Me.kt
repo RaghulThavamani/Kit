30b98104442e43735824d446f8f84b3509cb6bb7
RaghulThavamani <raghulthavamani@gmail.com>
2021-08-18 17:18:22 +0530

    Fun added

--- MYSDK/src/main/java/app/dreambig/mysdk/Me.kt
+++ MYSDK/src/main/java/app/dreambig/mysdk/Me.kt
@@ -2,8 +2,17 @@ package app.dreambig.mysdk
 
 class Me {
 
-    fun name() : String{
-        return "Iam Raghul"
+    fun name(name : String) : String{
+        return name
     }
+
+    fun role(role : String) : String{
+        return role
+    }
+
+    fun experience(experience : String) : String{
+        return experience
+    }
+
 
 }
